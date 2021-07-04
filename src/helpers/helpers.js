7624ba2c9bba7acfc43d149544be3047f7b3c1b0
pashapushak1996 <pashapushak1996@gmail.com>
2021-07-04 04:30:24 +0300

    29%

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,8 +1,4 @@
-const imageUrlCreator = (width, imageUrl) => {
-    if (imageUrl) {
-        return `https://image.tmdb.org/t/p/w${ width }${ imageUrl }`;
-    }
-};
+const imageUrlCreator = (width, imageUrl) => `https://image.tmdb.org/t/p/w${ width }${ imageUrl }`;
 
 export {
     imageUrlCreator
